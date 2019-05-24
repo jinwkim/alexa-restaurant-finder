71bb2c6c7f83915417e437a64ddb7f3719fc2fd9
Jin Kim <jinkim1195@gmail.com>
2019-05-24 00:53:02 -0400

    Update index.js
    Clean up API key for public view

--- index.js
+++ index.js
@@ -127,6 +127,8 @@ const ResultHandler = {
 			attributes.NameOfRestaurantTwo = APIresponse.businesses[1].name;
 			attributes.NameOfRestaurantThree = APIresponse.businesses[2].name;
 
+			// Code to POST the restaurant information to text to user
+
 			speakOutput += "I found some most reviewed restaurants from Yelp that are open right now. I recommend checking out ";
 			speakOutput += attributes.NameOfRestaurantOne + ". Perhaps check out " + attributes.NameOfRestaurantTwo + ". ";
 			speakOutput += "Or, " + attributes.NameOfRestaurantThree + ". Bon appetit!";
@@ -215,7 +217,7 @@ function httpGet(keyTerm,location) {
 		path: '/v3/businesses/search?term='+keyTerm+'&location='+location+'&limit=3&sort_by=review_count&open_now=true&radius=4800',
 		method: 'GET',
 		headers: {
-					Authorization: 'Bearer 5vLai0RfI-OP7kCK4041R9pu86fDydKYRs-K64YVdjEUunLnw508qogHf4ZGhTdSKJ6XYuXZJDxevR07pnSZlZT0jkKLM9b7TKQ19m0D0GUYYLHXl5gciIKYqfvlXHYx'
+					Authorization: 'Bearer token from Yelp goes here'
 				},
 	};
 	
